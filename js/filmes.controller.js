5ff8b680e37ca90a68371a31b6a3d6e98f1db001
Sabrina Silva <lima.sbrn@gmail.com>
2022-08-01 20:25:59 -0300

    add controllers in html/Scope creation

--- js/filmes.controller.js
+++ js/filmes.controller.js
@@ -2,6 +2,15 @@ angular.module("Filmes").controller("FilmesController", function($scope) {
 
     $scope.titulo = "Favorite movie List";
 
-    $scope.filmes = [];
+    $scope.filmes = [
+        {
+            id:"1",
+            titulo:"Tudo em todo lugar ao mesmo tempo",
+            ano: 2022,
+            produtora:"Diamond Filmes",
+            sinopse:"Cheio de ação e humor, conta a história de Evelyn Wang (Michelle Yeoh) uma mulher que se envolve por acaso em uma aventura multidimensional que coloca o destino de todos os universos em suas mãos - e também a faz questionar quem ela é para si mesma e sua família.",
+            cartaz:"https://br.web.img2.acsta.net/pictures/22/05/16/17/59/5165498.jpg"
+        }
+    ];
 
 });
